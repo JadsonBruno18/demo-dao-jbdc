507f90607624a9b7f2b6408e8268b8ecaacd8b3a
Santos <jadson.santos@capgemini.com>
2025-06-27 16:02:24 -0300

    seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,2 +1,15 @@
-package application;public class Program {
+package application;
+
+import entities.Department;
+import entities.Seller;
+
+import java.util.Date;
+
+public class Program {
+    public static void main(String[] args) {
+        Department obj = new Department(1,"Books");
+        Seller seller = new Seller(12, "Joao", "joao@hotmail.com", new Date(), 5000.00, obj);
+        System.out.println(seller);
+
+    }
 }
